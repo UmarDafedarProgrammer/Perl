e76ee0ba869685cb0a82a41a608fec5bf0c7def2
Umar Dafedar <108915275+UmarDafedarProgrammer@users.noreply.github.com>
2023-06-24 19:46:12 +0530

    Create ScalarData.pl

---
+++ ScalarData.pl
@@ -0,0 +1,11 @@
+#!/usr/bin/perl
+
+$Name = "Danerys Stormborn"; // String
+$Age = 34; // Integer/Number
+$Salary = 99999.89; // Float Number
+$Band = 'A'; / Character
+
+print("Name of the Employee is :", $Name,"\n");
+print("Age is : ",$Age, "\n");
+print("Band is : ",$Band,"\n");
+print("Salary is :",$Salary,"\n");
