841e2a48c6724d4edf7482f2bc6d90ddc492223c
Umar Dafedar <108915275+UmarDafedarProgrammer@users.noreply.github.com>
2023-06-26 18:49:19 +0530

    Create A_4_Numbers.pl

---
+++ A_4_Numbers.pl
@@ -0,0 +1,21 @@
+$ cat Numbers.pl
+#!/usr/bin/perl
+
+# Numbers - Whole Numbers, Floating point numbers/ decimal numbers
+# By Default any numbmer is 2 decimal floating point number
+# Decimal Representation, Exponential Representation, Floating point, Octal Representation, Binary Representation, Hexadecimal Representation
+# Exponential notation - 12e9
+# Binary Represenation - Starts with 0b
+# Hexadecimal Representation - Starts with 0x
+# Octal Representation - Starts with 0
+
+print(4+5,"\n");
+print(4.5 + 9,"\n");
+print(5.33234 + 6.59090,"\n");
+
+print(4*3e5,"\n");
+
+$val1 = 4e2;
+print($val1,"\n");
+$val1 = 4**2;
+print($val1,"\n");
