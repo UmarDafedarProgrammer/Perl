21f4d3ced19177129b2f0eb4a2a3777379d324ae
Umar Dafedar <108915275+UmarDafedarProgrammer@users.noreply.github.com>
2023-06-26 17:17:40 +0530

    Create A_3_StringLiterals.pl

---
+++ A_3_StringLiterals.pl
@@ -0,0 +1,26 @@
+#!/usr/bin/perl
+
+
+# String Literals
+# Single Quoted OR Double Quoted
+# Single quoted string literals are enclosed within single quotes, escape characters are not considered
+# Double Quoted string literals are enclosed within double quotes and escape characters are considered
+# In Single quoted string literlas, only the \' is considered to print single quote as part of the string
+
+$String1 = 'Daenerys Targarean \n\'';
+$String2 = "Drogon, Rhaegal, Viserion \n";
+$String3 = "Umar\t Dafedar\n Uzma \r Dafedar \f Anam D\b Dafedar\a Hurain \e";
+print($String1, $String2);
+print($String3);
+print("\a \a \a \a \a \a \a \a");
+
+# Escape Characters
+# \a - alarm
+# \n - next line
+# \t - tab
+# \r - Carriage Return
+# \b - backspace
+# \f - form feed
+# \\ - \
+# \' - Single quote
+# \e - escape
