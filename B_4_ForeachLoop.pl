3232aad5ca47a4bfc8f211483367cb2357165b13
Umar Dafedar <108915275+UmarDafedarProgrammer@users.noreply.github.com>
2023-07-09 17:13:50 +0530

    Create B_4_ForeachLoop.pl

---
+++ B_4_ForeachLoop.pl
@@ -0,0 +1,11 @@
+#!/usr/bin/perl
+
+
+@MyArray = (1,2,3,4,5,6,7,8,9,0,11);
+@SecondArray = qw(2 3 41 2 3 4 5 2 3 1);
+@ThirdArray = qw/hello perl 1 Script 3.4/;
+
+foreach $var (@MyArray)
+{
+        print("$var - ");
+}
