1634bf65ab988b19a86bd1d17c5f149346389f46
Umar Dafedar <108915275+UmarDafedarProgrammer@users.noreply.github.com>
2023-07-09 17:03:05 +0530

    Create B_2_list.pl

---
+++ B_2_list.pl
@@ -0,0 +1,14 @@
+#!/usr/bin/perl
+
+
+@my_array = qw/1 2 3 4 5 6 7 8 /;
+
+print("@my_array \n");
+
+($Val1, $Val2) = (1,2);
+
+print("$Val1 $Val2 \n");
+
+($v1, $v2, $v3) = (1,2,3,4,3,4,3,4,3,4);
+
+print("$v1 $v2 $v3 \n");
